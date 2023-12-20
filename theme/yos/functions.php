bcad0b3dbf096ff4be2b4de663eeb79749e38833
Stanislav-pr <stanislav.retinskiy@gmail.com>
2023-12-20 12:11:49 +0100

    wp

--- theme/yos/functions.php
+++ theme/yos/functions.php
@@ -1,51 +1,14 @@
-<?php 
+<?php
 
-add_action( 'wp_enqueue_scripts', 'add_styles' );
-add_action( 'wp_enqueue_scripts', 'add_scripts' );
+include 'inc/enqueue.php';     // add styles and scripts
+include 'inc/acf.php';         // custom acf functions
+include 'inc/extras.php';      // custom functions
+include 'classes/walker.php';  // walker nav menu
+include 'inc/register.php';    // custom ajax register and auth
+include 'inc/ajax-actions.php';// ajax actions
+
+
 add_action('after_setup_theme', 'theme_register_nav_menu');
-
-
-function add_styles() {
-	wp_enqueue_style('normalizecss', get_template_directory_uri().'/css/normalize.css');
-  wp_enqueue_style('fontscss', get_template_directory_uri().'/css/font.css');
-	wp_enqueue_style('fontawesomecss', get_template_directory_uri().'/fonts/fontawesome-5-pro-master/css/all.css');
-	wp_enqueue_style('fancyboxcss', get_template_directory_uri().'/css/jquery.fancybox.min.css');
-  wp_enqueue_style('swipercss', get_template_directory_uri().'/css/swiper.min.css' );
-  wp_enqueue_style('niceselectcss', get_template_directory_uri().'/css/nice-select.css');
-  wp_enqueue_style('styles', get_template_directory_uri().'/css/styles.css');
-	wp_enqueue_style('responsive', get_template_directory_uri().'/css/responsive.css');
-	wp_enqueue_style( 'theme', get_stylesheet_uri(), );
-
-}
-
-function add_scripts() {
-
-	wp_deregister_script('jquery');
-
-	wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), false, true);
-	wp_enqueue_script( 'fancyboxjs', get_template_directory_uri() . '/js/jquery.fancybox.min.js', array(), false, true);
-	wp_enqueue_script( 'swiperjs', get_template_directory_uri() . '/js/swiper.js', array(), false, true);
-	wp_enqueue_script( 'niceselectjs', get_template_directory_uri() . '/js/jquery.nice-select.min.js', array(), false, true);
-	wp_enqueue_script( 'cuttrjs', get_template_directory_uri() . '/js/cuttr.min.js', array(), false, true);
-	wp_enqueue_script( 'nicescrolljs', get_template_directory_uri() . '/js/jquery.nicescroll.min.js', array(), false, true);
-	wp_enqueue_script( 'maskjs', get_template_directory_uri() . '/js/jquery.mask.min.js', array(), false, true);
-	wp_enqueue_script( 'countdownjs', get_template_directory_uri() . '/js/countdown.js', array(), false, true);
-	wp_enqueue_script( 'stickyjs', get_template_directory_uri() . '/js/jquery.sticky.js', array(), false, true);
-	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), false, true);
-
-	// wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), false, true);
-
-	// wp_localize_script('script', 'global',
-	// 	array(
-	// 		'url' => admin_url('admin-ajax.php'),
-	// 		'template' => get_template_directory_uri()
-	// 	)
-	// );
-
-
-}
-
-
 
 
 function theme_register_nav_menu(){
@@ -63,29 +26,19 @@ if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page();
 
-	acf_add_options_sub_page('Header&Footer');
+	acf_add_options_sub_page('Theme Settings');
 }
 
 
-add_filter( 'body_class','my_class_names' );
-function my_class_names( $classes ) {
-
-	
-	// if( is_page(63) ){
-	// 	$classes[] = 'page page-contact';
-	// }
-	
-	return $classes;
-}
 
 function phone_clear($phone_num){ 
     $phone_num = preg_replace("![^0-9]+!",'',$phone_num);
     return($phone_num); 
 }				
-// <?php echo phone_clear(get_field('phone', 'options')); 
+
 
 function my_acf_init() {
-	acf_update_setting('google_api_key', 'AIzaSyAh1NE8kfXzx31UyPrwTCqwJdETUseulmI'); // Ваш ключ Google API
+	acf_update_setting('google_api_key', 'AIzaSyAh1NE8kfXzx31UyPrwTCqwJdETUseulmI');
 }
 
 add_action('acf/init', 'my_acf_init');
