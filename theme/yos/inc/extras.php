5e9bc86d4a33106f537000164566542272b94865
Stanislav-pr <stanislav.retinskiy@gmail.com>
2023-12-20 12:16:51 +0100

    Update extras.php

--- theme/yos/inc/extras.php
+++ theme/yos/inc/extras.php
@@ -1,12 +1,5 @@
 <?php
 
-/* clear phone number */
-
-function phone_clear($phone_num){
-    $phone_num = preg_replace("![^0-9]+!",'',$phone_num);
-    return($phone_num);
-}
-
 /* pagination markup */
 
 add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
