49fdfb51931fdbbc2158a60e3966d1a4d67ee726
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-01-09 10:07:57 +0100

    cart

--- theme/yos/parts/header/side-basket.php
+++ theme/yos/parts/header/side-basket.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <div class="side-basket" data-side-basket>
     <?php wc_get_template_part( 'cart/mini-cart' );?>
 </div>
--- theme/yos/woocommerce/cart/mini-cart.php
+++ theme/yos/woocommerce/cart/mini-cart.php
@@ -26,108 +26,122 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
     <div class="side-basket__container">
         <button class="side-basket__close-btn" data-action="close-side-basket"><span class="icon-close-thin"></span></button>
         <div class="side-basket__head">
-            ваш кошик <span>(3)</span>
+            <?= __('ваш кошик', 'yos');?> <span>(<?= WC()->cart->get_cart_contents_count();?>)</span>
         </div>
         <div class="side-basket__scroll-container">
             <ul class="side-basket__products-list">
-                <li>
-                    <div class="product-card-sm">
-                        <div class="product-card-sm__left">
-                            <button class="product-card-sm__btn-remove"><span class="icon-close"></span></button>
-                            <a href="#" class="product-card-sm__img">
-                                <img src="img/photo/product-card-img-1.png" alt="">
-                            </a>
-                        </div>
-                        <div class="product-card-sm__right">
-                            <div class="product-card-sm__title"><a href="#">zein obagi</a></div>
-                            <div class="product-card-sm__text">
-                                <div class="product-card-sm__text-1">
-                                    Exfoliating Polish, 65g
-                                </div>
-                                <div class="product-card-sm__text-2">
-                                    Zo Skin Health
-                                </div>
+                <?php do_action( 'woocommerce_before_mini_cart_contents' );
+
+                    foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+                        $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+                        $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+                        if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+                            /**
+                             * This filter is documented in woocommerce/templates/cart/cart.php.
+                             *
+                             * @since 2.1.0
+                             */
+                            $product_name      = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key );
+                            $thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+                            $product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
+                            $product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+				?>
+                    <li>
+                        <div class="product-card-sm">
+                            <div class="product-card-sm__left">
+                                <button class="product-card-sm__btn-remove"><span class="icon-close"></span></button>
+                                <a href="<?php echo esc_url( $product_permalink ); ?>" class="product-card-sm__img">
+                                    <img src="img/photo/product-card-img-1.png" alt="">
+                                </a>
                             </div>
-                            <div class="product-card-sm__group">
-                                <div class="product-card-sm__quantity">
-                                    <div class="product-card-sm__quantity-label">Кількість:</div>
-                                    <div class="quantity" data-quantity>
-                                        <button class="quantity__btn minus"><span class="icon-square-minus"></span></button>
-                                        <input type="text" value="1" class="quantity__value">
-                                        <button class="quantity__btn plus"><span class="icon-square-plus"></span></button>
+                            <div class="product-card-sm__right">
+                                <div class="product-card-sm__title"><a href="#">zein obagi</a></div>
+                                <div class="product-card-sm__text">
+                                    <div class="product-card-sm__text-1">
+                                        Exfoliating Polish, 65g
+                                    </div>
+                                    <div class="product-card-sm__text-2">
+                                        Zo Skin Health
                                     </div>
                                 </div>
-                                <div class="product-card-sm__price">
-                                    <div class="product-card-sm__price-current">3 199 ₴</div>
-                                    <div class="product-card-sm__price-old">7 299 ₴</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product-card-sm">
-                        <div class="product-card-sm__left">
-                            <button class="product-card-sm__btn-remove"><span class="icon-close"></span></button>
-                            <a href="#" class="product-card-sm__img">
-                                <img src="img/photo/product-card-img-2.png" alt="">
-                            </a>
-                        </div>
-                        <div class="product-card-sm__right">
-                            <div class="product-card-sm__title"><a href="#">rare paris</a></div>
-                            <div class="product-card-sm__text">
-                                <div class="product-card-sm__text-1">
-                                    Ecological Cellulose, 65g
-                                </div>
-                                <div class="product-card-sm__text-2">
-                                    Tresor Solaire
-                                </div>
-                            </div>
-                            <div class="product-card-sm__group">
-                                <div class="product-card-sm__quantity">
-                                    <div class="product-card-sm__quantity-label">Кількість:</div>
-                                    <div class="quantity" data-quantity>
-                                        <button class="quantity__btn minus"><span class="icon-square-minus"></span></button>
-                                        <input type="text" value="1" class="quantity__value">
-                                        <button class="quantity__btn plus"><span class="icon-square-plus"></span></button>
+                                <div class="product-card-sm__group">
+                                    <div class="product-card-sm__quantity">
+                                        <div class="product-card-sm__quantity-label">Кількість:</div>
+                                        <div class="quantity" data-quantity>
+                                            <button class="quantity__btn minus"><span class="icon-square-minus"></span></button>
+                                            <input type="text" value="1" class="quantity__value">
+                                            <button class="quantity__btn plus"><span class="icon-square-plus"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="product-card-sm__price">
+                                        <div class="product-card-sm__price-current">3 199 ₴</div>
+                                        <div class="product-card-sm__price-old">7 299 ₴</div>
                                     </div>
                                 </div>
-                                <div class="product-card-sm__price">
-                                    <div class="product-card-sm__price-current">299 ₴</div>
-                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
+
+                            <li class="woocommerce-mini-cart-item <?php echo esc_attr( apply_filters( 'woocommerce_mini_cart_item_class', 'mini_cart_item', $cart_item, $cart_item_key ) ); ?>">
+                                <?php
+                                echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                                    'woocommerce_cart_item_remove_link',
+                                    sprintf(
+                                        '<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
+                                        esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+                                        /* translators: %s is the product name */
+                                        esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
+                                        esc_attr( $product_id ),
+                                        esc_attr( $cart_item_key ),
+                                        esc_attr( $_product->get_sku() )
+                                    ),
+                                    $cart_item_key
+                                );
+                                ?>
+                                <?php if ( empty( $product_permalink ) ) : ?>
+                                    <?php echo $thumbnail . wp_kses_post( $product_name ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                                <?php else : ?>
+                                    <a href="<?php echo esc_url( $product_permalink ); ?>">
+                                        <?php echo $thumbnail . wp_kses_post( $product_name ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                                    </a>
+                                <?php endif; ?>
+                                <?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                                <?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                            </li>
+                <?php }
+                    }
+
+                do_action( 'woocommerce_mini_cart_contents' );
+                ?>
             </ul>
 
             <div class="side-basket__payment-info">
                 <div class="side-basket__payment-info-row">
-                    <span>Знижка за системою лояльності</span>
+                    <span><?= __('Знижка за системою лояльності', 'yos');?></span>
                     <span class="text-nowrap">-60 ₴</span>
                 </div>
                 <div class="side-basket__payment-info-row">
-                    <span>Інші знижки</span>
+                    <span><?= __('Інші знижки', 'yos');?></span>
                     <span class="text-nowrap">0 ₴</span>
                 </div>
                 <div class="side-basket__payment-info-row">
-                    <span>Доставка</span>
-                    <span>За тарифами перевізника</span>
+                    <span><?= __('Доставка', 'yos');?></span>
+                    <span><?= __('За тарифами перевізника', 'yos');?></span>
                 </div>
                 <div class="side-basket__payment-info-row side-basket__payment-info-row--total">
-                    <span>всього до сплати</span>
+                    <span><?= __('всього до сплати', 'yos');?></span>
                     <span class="text-nowrap">4 098 ₴</span>
                 </div>
             </div>
 
             <div class="side-basket__to-checkout">
-                <a href="#" class="button-primary dark w-100">перейти далі</a>
+                <a href="#" class="button-primary dark w-100"><?= __('перейти далі', 'yos');?></a>
             </div>
 
             <div class="side-basket__free-shipping">
                 <div class="side-basket__free-shipping-head">
-                    <span>До безкоштовної доставки
-                        залишилось:</span>
+                    <span><?= __('До безкоштовної доставки залишилось:', 'yos');?></span>
                     <span class="text-nowrap">1 230 ₴</span>
                 </div>
                 <div class="side-basket__free-shipping-line">
@@ -143,58 +157,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
         </div>
     </div>
 
-	<ul class="woocommerce-mini-cart cart_list product_list_widget <?php echo esc_attr( $args['list_class'] ); ?>">
-		<?php
-		do_action( 'woocommerce_before_mini_cart_contents' );
 
-		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-			$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-			$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
-
-			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-				/**
-				 * This filter is documented in woocommerce/templates/cart/cart.php.
-				 *
-				 * @since 2.1.0
-				 */
-				$product_name      = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key );
-				$thumbnail         = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
-				$product_price     = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
-				$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
-				?>
-				<li class="woocommerce-mini-cart-item <?php echo esc_attr( apply_filters( 'woocommerce_mini_cart_item_class', 'mini_cart_item', $cart_item, $cart_item_key ) ); ?>">
-					<?php
-					echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-						'woocommerce_cart_item_remove_link',
-						sprintf(
-							'<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
-							esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-							/* translators: %s is the product name */
-							esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
-							esc_attr( $product_id ),
-							esc_attr( $cart_item_key ),
-							esc_attr( $_product->get_sku() )
-						),
-						$cart_item_key
-					);
-					?>
-					<?php if ( empty( $product_permalink ) ) : ?>
-						<?php echo $thumbnail . wp_kses_post( $product_name ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					<?php else : ?>
-						<a href="<?php echo esc_url( $product_permalink ); ?>">
-							<?php echo $thumbnail . wp_kses_post( $product_name ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-						</a>
-					<?php endif; ?>
-					<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</li>
-				<?php
-			}
-		}
-
-		do_action( 'woocommerce_mini_cart_contents' );
-		?>
-	</ul>
 
 	<p class="woocommerce-mini-cart__total total">
 		<?php
