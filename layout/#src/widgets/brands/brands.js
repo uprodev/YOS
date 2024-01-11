6044a0521a5e31ebf454c332f2abc340092a1daf
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-01-11 11:39:35 +0200

    brands page changed

--- layout/#src/widgets/brands/brands.js
+++ layout/#src/widgets/brands/brands.js
@@ -1,45 +1,9 @@
-const bestBrandsSlider = document.querySelector('[data-slider="best-brands"]');
-if (bestBrandsSlider) {
-    const slider = bestBrandsSlider;
-    let mySwiper;
-
-    function mobileSlider() {
-        if (document.documentElement.clientWidth <= 767 && slider.dataset.mobile == 'false') {
-            mySwiper = new Swiper(slider, {
-                slidesPerView: 'auto',
-                freeMode: true,
-                scrollbar: {
-                    el: slider.querySelector('.swiper-scrollbar'),
-                },
-            });
-
-            slider.dataset.mobile = 'true';
-        }
-
-        if (document.documentElement.clientWidth > 767) {
-            slider.dataset.mobile = 'false';
-
-            if (slider.classList.contains('swiper-initialized')) {
-                mySwiper.destroy();
-            }
-        }
-    }
-
-    mobileSlider();
-
-    window.addEventListener('resize', () => {
-        mobileSlider();
-    })
-}
 
 const brandsNavSlider = document.querySelector('[data-slider="brands-nav"]');
 if (brandsNavSlider) {
     new Swiper(brandsNavSlider, {
         slidesPerView: 'auto',
-        freeMode: true,
-        scrollbar: {
-            el: brandsNavSlider.querySelector('.swiper-scrollbar'),
-        },
+        freeMode: true
     });
 }
 
--- layout/dist/js/app.js
+++ layout/dist/js/app.js
@@ -356,130 +356,130 @@ class Utils {
 
 class DynamicAdapt {
 	constructor(type) {
-	  this.type = type;
+		this.type = type;
 	}
-  
+
 	init() {
-	  this.оbjects = [];
-	  this.daClassname = '_dynamic_adapt_';
-	  this.nodes = [...document.querySelectorAll('[data-da]')];
-  
-	  this.nodes.forEach((node) => {
-		const data = node.dataset.da.trim();
-		const dataArray = data.split(',');
-		const оbject = {};
-		оbject.element = node;
-		оbject.parent = node.parentNode;
-		оbject.destination = document.querySelector(`${dataArray[0].trim()}`);
-		оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
-		оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
-		оbject.index = this.indexInParent(оbject.parent, оbject.element);
-		this.оbjects.push(оbject);
-	  });
-  
-	  this.arraySort(this.оbjects);
-  
-	  this.mediaQueries = this.оbjects
-		.map(({
-		  breakpoint
-		}) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)
-		.filter((item, index, self) => self.indexOf(item) === index);
-  
-	  this.mediaQueries.forEach((media) => {
-		const mediaSplit = media.split(',');
-		const matchMedia = window.matchMedia(mediaSplit[0]);
-		const mediaBreakpoint = mediaSplit[1];
-  
-		const оbjectsFilter = this.оbjects.filter(
-		  ({
-			breakpoint
-		  }) => breakpoint === mediaBreakpoint
-		);
-		matchMedia.addEventListener('change', () => {
-		  this.mediaHandler(matchMedia, оbjectsFilter);
+		this.оbjects = [];
+		this.daClassname = '_dynamic_adapt_';
+		this.nodes = [...document.querySelectorAll('[data-da]')];
+
+		this.nodes.forEach((node) => {
+			const data = node.dataset.da.trim();
+			const dataArray = data.split(',');
+			const оbject = {};
+			оbject.element = node;
+			оbject.parent = node.parentNode;
+			оbject.destination = document.querySelector(`${dataArray[0].trim()}`);
+			оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
+			оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
+			оbject.index = this.indexInParent(оbject.parent, оbject.element);
+			this.оbjects.push(оbject);
 		});
-		this.mediaHandler(matchMedia, оbjectsFilter);
-	  });
+
+		this.arraySort(this.оbjects);
+
+		this.mediaQueries = this.оbjects
+			.map(({
+				breakpoint
+			}) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)
+			.filter((item, index, self) => self.indexOf(item) === index);
+
+		this.mediaQueries.forEach((media) => {
+			const mediaSplit = media.split(',');
+			const matchMedia = window.matchMedia(mediaSplit[0]);
+			const mediaBreakpoint = mediaSplit[1];
+
+			const оbjectsFilter = this.оbjects.filter(
+				({
+					breakpoint
+				}) => breakpoint === mediaBreakpoint
+			);
+			matchMedia.addEventListener('change', () => {
+				this.mediaHandler(matchMedia, оbjectsFilter);
+			});
+			this.mediaHandler(matchMedia, оbjectsFilter);
+		});
 	}
-  
+
 	mediaHandler(matchMedia, оbjects) {
-	  if (matchMedia.matches) {
-		оbjects.forEach((оbject) => {
-		  оbject.index = this.indexInParent(оbject.parent, оbject.element);
-		  this.moveTo(оbject.place, оbject.element, оbject.destination);
-		});
-	  } else {
-		оbjects.forEach(
-		  ({ parent, element, index }) => {
-			if (element.classList.contains(this.daClassname)) {
-			  this.moveBack(parent, element, index);
-			}
-		  }
-		);
-	  }
+		if (matchMedia.matches) {
+			оbjects.forEach((оbject) => {
+				оbject.index = this.indexInParent(оbject.parent, оbject.element);
+				this.moveTo(оbject.place, оbject.element, оbject.destination);
+			});
+		} else {
+			оbjects.forEach(
+				({ parent, element, index }) => {
+					if (element.classList.contains(this.daClassname)) {
+						this.moveBack(parent, element, index);
+					}
+				}
+			);
+		}
 	}
-  
+
 	moveTo(place, element, destination) {
-	  element.classList.add(this.daClassname);
-	  if (place === 'last' || place >= destination.children.length) {
-		destination.append(element);
-		return;
-	  }
-	  if (place === 'first') {
-		destination.prepend(element);
-		return;
-	  }
-	  destination.children[place].before(element);
+		element.classList.add(this.daClassname);
+		if (place === 'last' || place >= destination.children.length) {
+			destination.append(element);
+			return;
+		}
+		if (place === 'first') {
+			destination.prepend(element);
+			return;
+		}
+		destination.children[place].before(element);
 	}
-  
+
 	moveBack(parent, element, index) {
-	  element.classList.remove(this.daClassname);
-	  if (parent.children[index] !== undefined) {
-		parent.children[index].before(element);
-	  } else {
-		parent.append(element);
-	  }
+		element.classList.remove(this.daClassname);
+		if (parent.children[index] !== undefined) {
+			parent.children[index].after(element);
+		} else {
+			parent.append(element);
+		}
 	}
-  
+
 	indexInParent(parent, element) {
-	  return [...parent.children].indexOf(element);
+		return [...parent.children].indexOf(element);
 	}
-  
+
 	arraySort(arr) {
-	  if (this.type === 'min') {
-		arr.sort((a, b) => {
-		  if (a.breakpoint === b.breakpoint) {
-			if (a.place === b.place) {
-			  return 0;
-			}
-			if (a.place === 'first' || b.place === 'last') {
-			  return -1;
-			}
-			if (a.place === 'last' || b.place === 'first') {
-			  return 1;
-			}
-			return a.place - b.place;
-		  }
-		  return a.breakpoint - b.breakpoint;
-		});
-	  } else {
-		arr.sort((a, b) => {
-		  if (a.breakpoint === b.breakpoint) {
-			if (a.place === b.place) {
-			  return 0;
-			}
-			if (a.place === 'first' || b.place === 'last') {
-			  return 1;
-			}
-			if (a.place === 'last' || b.place === 'first') {
-			  return -1;
-			}
-			return b.place - a.place;
-		  }
-		  return b.breakpoint - a.breakpoint;
-		});
-		return;
-	  }
+		if (this.type === 'min') {
+			arr.sort((a, b) => {
+				if (a.breakpoint === b.breakpoint) {
+					if (a.place === b.place) {
+						return 0;
+					}
+					if (a.place === 'first' || b.place === 'last') {
+						return -1;
+					}
+					if (a.place === 'last' || b.place === 'first') {
+						return 1;
+					}
+					return a.place - b.place;
+				}
+				return a.breakpoint - b.breakpoint;
+			});
+		} else {
+			arr.sort((a, b) => {
+				if (a.breakpoint === b.breakpoint) {
+					if (a.place === b.place) {
+						return 0;
+					}
+					if (a.place === 'first' || b.place === 'last') {
+						return 1;
+					}
+					if (a.place === 'last' || b.place === 'first') {
+						return -1;
+					}
+					return b.place - a.place;
+				}
+				return b.breakpoint - a.breakpoint;
+			});
+			return;
+		}
 	}
 }
 ;
@@ -2004,48 +2004,12 @@ if (sideBasket) {
         }
     }
 };
-			const bestBrandsSlider = document.querySelector('[data-slider="best-brands"]');
-if (bestBrandsSlider) {
-    const slider = bestBrandsSlider;
-    let mySwiper;
-
-    function mobileSlider() {
-        if (document.documentElement.clientWidth <= 767 && slider.dataset.mobile == 'false') {
-            mySwiper = new Swiper(slider, {
-                slidesPerView: 'auto',
-                freeMode: true,
-                scrollbar: {
-                    el: slider.querySelector('.swiper-scrollbar'),
-                },
-            });
-
-            slider.dataset.mobile = 'true';
-        }
-
-        if (document.documentElement.clientWidth > 767) {
-            slider.dataset.mobile = 'false';
-
-            if (slider.classList.contains('swiper-initialized')) {
-                mySwiper.destroy();
-            }
-        }
-    }
-
-    mobileSlider();
-
-    window.addEventListener('resize', () => {
-        mobileSlider();
-    })
-}
-
+			
 const brandsNavSlider = document.querySelector('[data-slider="brands-nav"]');
 if (brandsNavSlider) {
     new Swiper(brandsNavSlider, {
         slidesPerView: 'auto',
-        freeMode: true,
-        scrollbar: {
-            el: brandsNavSlider.querySelector('.swiper-scrollbar'),
-        },
+        freeMode: true
     });
 }
 
