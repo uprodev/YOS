dbc4efce3c4e8e4d08c86a4f028233c838aaa3cd
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-01-15 15:54:11 +0100

    Create brands.php

---
+++ theme/yos/templates/sections/brands.php
@@ -0,0 +1,38 @@
+<?php
+
+$brands = get_sub_field('brands');
+
+?>
+
+<div class="top-space-140 top-space-md-150">
+    <section class="ticker-logos" data-ticker-logos>
+        <div dir="ltr" class="swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($brands as $brand):
+                    $name = get_term( $brand );
+                    $logo = get_field('logo_brand', 'pa_brand_'.$brand);?>
+                    <div class="swiper-slide">
+                        <a href="/shop/?action=filter&pa_brand=<?= $brand;?>" class="ticker-logo">
+                            <img src="<?= $logo;?>" alt="<?= $brand->name;?>">
+                        </a>
+                    </div>
+                <?php endforeach;?>
+
+            </div>
+        </div>
+        <div dir="rtl" class="swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($brands as $brand):
+                    $name = get_term( $brand );
+                    $logo = get_field('logo_brand', 'pa_brand_'.$brand);?>
+                    <div class="swiper-slide">
+                        <a href="/shop/?action=filter&pa_brand=<?= $brand;?>" class="ticker-logo">
+                            <img src="<?= $logo;?>" alt="<?= $brand->name;?>">
+                        </a>
+                    </div>
+                <?php endforeach;?>
+
+            </div>
+        </div>
+    </section>
+</div>
