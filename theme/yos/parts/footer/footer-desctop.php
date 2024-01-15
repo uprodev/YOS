60ad474a9bff7580c16f86298963a0dc03765e96
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-01-15 17:40:30 +0100

    Update footer-desctop.php

--- theme/yos/parts/footer/footer-desctop.php
+++ theme/yos/parts/footer/footer-desctop.php
@@ -3,22 +3,25 @@
         <div class="footer-desk__body">
             <div class="footer-desk__col footer-desk__col-1">
                 <div class="footer-desk__logo">
-                    <a href="#">
-                        <img src="img/logo/footer-logo.png" alt="">
+                    <a href="<?= get_home_url();?>">
+                        <?php $logo = get_field('footer_logo', 'options');?>
+                        <img src="<?= $logo['url'];?>" alt="<?= $logo['alt'];?>">
                     </a>
                 </div>
-                <div class="footer-desk__text text-content last-no-margin">
-                    <p>
-                        — це мультибрендовий онлайн beauty бутік, доступний для візиту з будь-якої точки світу.
-                    </p>
-                </div>
-                <div class="footer-desk__copy">
-                    © 2022 YOS Skincare. Всі права захищені. Designed & developed by ra-web
-                </div>
+                <?php if(get_field('footer_text', 'options')):?>
+                    <div class="footer-desk__text text-content last-no-margin">
+                        <p><?php the_field('footer_text', 'options');?></p>
+                    </div>
+                <?php endif;?>
+                <?php if(get_field('copyright', 'options')):?>
+                    <div class="footer-desk__copy">
+                        <?php the_field('copyright', 'options');?>
+                    </div>
+                <?php endif;?>
             </div>
             <div class="footer-desk__col footer-desk__col-2">
                 <h2 class="footer-desk__title title-2">
-                    <a href="#">категорії</a>
+                    <?php the_field('footer_title_1', 'options');?>
                 </h2>
                 <ul class="footer-desk__list">
                     <li>
@@ -49,26 +52,31 @@
             </div>
             <div class="footer-desk__col footer-desk__col-3">
                 <h2 class="footer-desk__title title-2">
-                    <a href="#">зв’яжіться з нами</a>
+                    <?php the_field('footer_title_2', 'options');?>
                 </h2>
                 <ul class="footer-desk__list">
-                    <li>
-                        з 10.00 до 18.00
-                    </li>
-                    <li>
-                        <a href="#">+380 (93) 28 55 810</a>
-                    </li>
-                    <li>
-                        <a href="#" class="button-link"><span>telegram</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="button-link"><span>instagram</span></a>
-                    </li>
+                    <?php if(get_field('footer_hours', 'options')):?>
+                        <li>
+                            <?php the_field('footer_hours', 'options');?>
+                        </li>
+                    <?php endif;?>
+                    <?php if(get_field('footer_phone', 'options')):?>
+                        <li>
+                            <a href="+<?= phone_clear(get_field('footer_phone', 'options'));?>"><?php the_field('footer_phone', 'options');?></a>
+                        </li>
+                    <?php endif;?>
+                    <?php if(get_field('footer_social', 'options')):
+                        foreach (get_field('footer_social', 'options') as $soc):?>
+                            <li>
+                                <a href="<?= $soc['link'];?>" target="_blank" class="button-link"><span><?= $soc['name'];?></span></a>
+                            </li>
+                        <?php endforeach;
+                    endif;?>
                 </ul>
             </div>
             <div class="footer-desk__col footer-desk__col-4">
                 <h2 class="footer-desk__title title-2">
-                    <a href="#">інформація</a>
+                    <?php the_field('footer_title_3', 'options');?>
                 </h2>
                 <ul class="footer-desk__list">
                     <li>
