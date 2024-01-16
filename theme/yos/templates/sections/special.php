872463b2e7e1cf6521fc798d193f4674ab4a5a33
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-01-16 20:19:11 +0100

    Update special.php

--- theme/yos/templates/sections/special.php
+++ theme/yos/templates/sections/special.php
@@ -1,7 +1,7 @@
 <?php
 
 $title = get_sub_field('title');
-$text = get_sub_field('text');
+$proposes = get_sub_field('proposes');
 
 ?>
 
@@ -15,236 +15,48 @@ $text = get_sub_field('text');
                     </div>
                 <?php endif;?>
             </div>
-            <div class="swiper" data-slider="carousel">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide big-card">
-                        <div class="special-offer-card" data-special-offer-card>
-                            <div class="special-offer-card__img ibg">
-                                <img src="img/photo/special-offer-img-1.jpg" alt="">
-                            </div>
-                            <div class="special-offer-card__text-content">
-                                <h2 class="special-offers__title title-2"><a href="#">спеціальні пропозиції</a></h2>
-                                <div class="special-offer-card__text" data-truncate-string="120">
-                                    Безкоштовна доставка
-                                    замовлень Новою Поштою по Україні на суму від 2 000 ₴.
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sint fugiat corrupti natus delectus excepturi doloribus non minima, ad quam.
-                                </div>
-                                <a href="#" class="button-link" data-action="show-full-text-by-index" data-index="0">
-                                    <span>детальніше</span>
-                                </a>
-                                <div class="special-offer-card__full-text" data-index="0">
-                                    <div class="special-offer-card__text text-content last-no-margin">
-                                        <p>
-                                            Повнорозмірний продукт для рук у подарунок
-                                            при купівлі PRODUCT BRAND від 5 000 ₴.
-                                        </p>
-                                        <p>
-                                            При наявності акційного товару. Ціни на сайті
-                                            вказані з урахуванням знижки. В акції беруть
-                                            участь товари, що доставляються зі складу в
-                                            Україні.
-                                        </p>
-                                        <ul>
-                                            <li>
-                                                * кількість подарунків обмежена
-                                            </li>
-                                            <li>
-                                                * до закінчення акції залишилось 2 дні
-                                            </li>
-                                        </ul>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
+            <?php if($proposes): $p=1;?>
+                <div class="swiper" data-slider="carousel">
+                    <div class="swiper-wrapper">
+                        <?php foreach($proposes as $prop):?>
+                            <div class="swiper-slide <?= ($p==1||$p%3==1)?'big-card':'';?>">
+                                <div class="special-offer-card" data-special-offer-card>
+                                    <div class="special-offer-card__img ibg">
+                                        <img src="<?= $prop['image'];?>" alt="<?= $prop['title'];?>">
                                     </div>
-
-                                    <a href="#" class="button-link"
-                                       data-action="hide-full-text-by-index"><span>закрити</span></a>
+                                    <div class="special-offer-card__text-content">
+                                        <?php if($p==1):?>
+                                            <h2 class="special-offers__title title-2"><?= $title;?></h2>
+                                        <?php endif;?>
+                                        <div class="special-offer-card__text" data-truncate-string="120">
+                                            <?= $prop['text'];?>
+                                        </div>
+                                        <a href="#" class="button-link" data-action="show-full-text-by-index" data-index="0">
+                                            <span><?= __('детальніше', 'yos');?></span>
+                                        </a>
+                                        <div class="special-offer-card__full-text" data-index="0">
+                                            <div class="special-offer-card__text text-content last-no-margin">
+                                                <?= $prop['full_text'];?>
+                                            </div>
+                                            <a href="#" class="button-link" data-action="hide-full-text-by-index"><span><?= __('закрити', 'yos');?></span></a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        <?php $p++; endforeach;?>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="special-offer-card" data-special-offer-card>
-                            <div class="special-offer-card__img ibg">
-                                <img src="img/photo/special-offer-img-2.jpg" alt="">
-                            </div>
-                            <div class="special-offer-card__text-content">
-                                <div class="special-offer-card__text" data-truncate-string="120">
-                                    Повнорозмірний продукт для рук у подарунок
-                                    при купівлі PRODUCT BRAND від 5 000 ₴.
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic provident temporibus voluptas, molestiae quae nemo in quaerat! Magnam, beatae quidem.
-                                </div>
-                                <a href="#" class="button-link" data-action="show-full-text-by-index" data-index="0">
-                                    <span>детальніше</span>
-                                </a>
-                                <div class="special-offer-card__full-text" data-index="0">
-                                    <div class="special-offer-card__text text-content last-no-margin">
-                                        <p>
-                                            Повнорозмірний продукт для рук у подарунок
-                                            при купівлі PRODUCT BRAND від 5 000 ₴.
-                                        </p>
-                                        <p>
-                                            При наявності акційного товару. Ціни на сайті
-                                            вказані з урахуванням знижки. В акції беруть
-                                            участь товари, що доставляються зі складу в
-                                            Україні.
-                                        </p>
-                                        <ul>
-                                            <li>
-                                                * кількість подарунків обмежена
-                                            </li>
-                                            <li>
-                                                * до закінчення акції залишилось 2 дні
-                                            </li>
-                                        </ul>
-                                    </div>
-
-                                    <a href="#" class="button-link"
-                                       data-action="hide-full-text-by-index"><span>закрити</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="special-offer-card" data-special-offer-card>
-                            <div class="special-offer-card__img ibg">
-                                <img src="img/photo/special-offer-img-3.jpg" alt="">
-                            </div>
-                            <div class="special-offer-card__text-content">
-                                <div class="special-offer-card__text" data-truncate-string="120">
-                                    Безкоштовна доставка замовлень Новою
-                                    Поштою по Україні на суму від 2 000 ₴.
-                                </div>
-                                <a href="#" class="button-link" data-action="show-full-text-by-index" data-index="0">
-                                    <span>детальніше</span>
-                                </a>
-                                <div class="special-offer-card__full-text" data-index="0">
-                                    <div class="special-offer-card__text text-content last-no-margin">
-                                        <p>
-                                            Повнорозмірний продукт для рук у подарунок
-                                            при купівлі PRODUCT BRAND від 5 000 ₴.
-                                        </p>
-                                        <p>
-                                            При наявності акційного товару. Ціни на сайті
-                                            вказані з урахуванням знижки. В акції беруть
-                                            участь товари, що доставляються зі складу в
-                                            Україні.
-                                        </p>
-                                        <ul>
-                                            <li>
-                                                * кількість подарунків обмежена
-                                            </li>
-                                            <li>
-                                                * до закінчення акції залишилось 2 дні
-                                            </li>
-                                        </ul>
-                                    </div>
-
-                                    <a href="#" class="button-link"
-                                       data-action="hide-full-text-by-index"><span>закрити</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide big-card">
-                        <div class="special-offer-card" data-special-offer-card>
-                            <div class="special-offer-card__img ibg">
-                                <img src="img/photo/special-offer-img-1.jpg" alt="">
-                            </div>
-                            <div class="special-offer-card__text-content">
-                                <div class="special-offer-card__text" data-truncate-string="120">
-                                    Безкоштовна доставка
-                                    замовлень Новою Поштою по Україні на суму від 2 000 ₴.
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sint fugiat corrupti natus delectus excepturi doloribus non minima, ad quam.
-                                </div>
-                                <a href="#" class="button-link" data-action="show-full-text-by-index" data-index="0">
-                                    <span>детальніше</span>
-                                </a>
-                                <div class="special-offer-card__full-text" data-index="0">
-                                    <div class="special-offer-card__text text-content last-no-margin">
-                                        <p>
-                                            Повнорозмірний продукт для рук у подарунок
-                                            при купівлі PRODUCT BRAND від 5 000 ₴.
-                                        </p>
-                                        <p>
-                                            При наявності акційного товару. Ціни на сайті
-                                            вказані з урахуванням знижки. В акції беруть
-                                            участь товари, що доставляються зі складу в
-                                            Україні.
-                                        </p>
-                                        <ul>
-                                            <li>
-                                                * кількість подарунків обмежена
-                                            </li>
-                                            <li>
-                                                * до закінчення акції залишилось 2 дні
-                                            </li>
-                                        </ul>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium eum
-                                            animi reprehenderit atque magni itaque incidunt! Laborum, quod eius?
-                                        </p>
-                                    </div>
-
-                                    <a href="#" class="button-link"
-                                       data-action="hide-full-text-by-index"><span>закрити</span></a>
-                                </div>
+                    <div class="carousel__navigation">
+                        <div class="slider-navigations">
+                            <div class="swiper-scrollbar slider-scrollbar"></div>
+                            <div class="slider-buttons">
+                                <button class="slider-btn left"><span class="icon-arrow-left"></span></button>
+                                <div class="slider-pagination"></div>
+                                <button class="slider-btn right"><span class="icon-arrow-right"></span></button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="carousel__navigation">
-                    <div class="slider-navigations">
-                        <div class="swiper-scrollbar slider-scrollbar"></div>
-                        <div class="slider-buttons">
-                            <button class="slider-btn left"><span class="icon-arrow-left"></span></button>
-                            <div class="slider-pagination"></div>
-                            <button class="slider-btn right"><span class="icon-arrow-right"></span></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php endif;?>
         </div>
     </div>
 </div>
