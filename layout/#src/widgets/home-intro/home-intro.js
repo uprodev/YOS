d1dcbfaf3b8981355f885030eefaafcf3b32df7c
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-01-24 16:58:24 +0200

    trello fixes

--- layout/#src/widgets/home-intro/home-intro.js
+++ layout/#src/widgets/home-intro/home-intro.js
@@ -5,11 +5,25 @@ if(homeIntro) {
         speed: 600,
         scrollbar: {
             el: homeIntro.querySelector('.swiper-scrollbar'),
-            draggable: true
+            draggable: true,
         },
         navigation: {
             nextEl: homeIntro.querySelector('.slider-btn.right'),
             prevEl: homeIntro.querySelector('.slider-btn.left'),
         },
+    })
+
+    const scrollbar = homeIntro.querySelector('.swiper-scrollbar');
+    if(!scrollbar) return;
+    const clickLine = document.createElement('div');
+    clickLine.className = 'click-line';
+    scrollbar.append(clickLine);
+    
+    clickLine.addEventListener('pointerdown', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const countOfSlides = swiperSlider.slides.length;
+        const step = clickLine.clientWidth / countOfSlides
+        swiperSlider.slideTo(Math.floor(e.layerX / step));
     })
 }
