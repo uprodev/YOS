7fc9761e80997466e749835181c17df2be1c2b07
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-01-25 22:11:23 +0100

    Update footer-desctop.php

--- theme/yos/parts/footer/footer-desctop.php
+++ theme/yos/parts/footer/footer-desctop.php
@@ -23,32 +23,11 @@
                 <h2 class="footer-desk__title title-2">
                     <?php the_field('footer_title_1', 'options');?>
                 </h2>
-                <ul class="footer-desk__list">
-                    <li>
-                        <a href="#">Обличчя</a>
-                    </li>
-                    <li>
-                        <a href="#">Волосся</a>
-                    </li>
-                    <li>
-                        <a href="#">Тіло</a>
-                    </li>
-                    <li>
-                        <a href="#">Аксесуари</a>
-                    </li>
-                    <li>
-                        <a href="#">Подарунки</a>
-                    </li>
-                    <li>
-                        <a href="#">Health & Care</a>
-                    </li>
-                    <li>
-                        <a href="#">Sale</a>
-                    </li>
-                    <li>
-                        <a href="#">Бренди</a>
-                    </li>
-                </ul>
+                <?php wp_nav_menu([
+                    'theme_location' => 'footer-category',
+                    'container' => false,
+                    'menu_class' => 'footer-desk__list',
+                ]);?>
             </div>
             <div class="footer-desk__col footer-desk__col-3">
                 <h2 class="footer-desk__title title-2">
@@ -78,20 +57,11 @@
                 <h2 class="footer-desk__title title-2">
                     <?php the_field('footer_title_3', 'options');?>
                 </h2>
-                <ul class="footer-desk__list">
-                    <li>
-                        <a href="#">Угода користувача</a>
-                    </li>
-                    <li>
-                        <a href="#">Справжність товару</a>
-                    </li>
-                    <li>
-                        <a href="#">Повернення товару</a>
-                    </li>
-                    <li>
-                        <a href="#">Доставка і оплата</a>
-                    </li>
-                </ul>
+                <?php wp_nav_menu([
+                    'theme_location' => 'footer-info',
+                    'container' => false,
+                    'menu_class' => 'footer-desk__list',
+                ]);?>
             </div>
         </div>
     </div>
