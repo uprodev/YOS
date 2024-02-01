6545b63f7e5be350cc77b68ce9fd6fa453dccbae
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-02-01 15:35:13 +0200

    app.js updated

--- theme/yos/js/app.js
+++ theme/yos/js/app.js
@@ -1006,17 +1006,20 @@ if (productImages) {
         const parent = btn.parentElement;
         const input = parent.querySelector('.quantity__value');
 
+        const event = new Event('input', { bubbles: true });
+
         if(btn.classList.contains('minus')) {
             if(input.value <= 1) return;
             
             input.value--;
+            input.dispatchEvent(event);
 
             if(input.value == 1) {
                 btn.classList.add('disabled');
             }
         } else if(btn.classList.contains('plus')) {
             input.value++;
-
+            input.dispatchEvent(event);
             const btnMinus = parent.querySelector('.quantity__btn.minus');
             if(!btnMinus) return;
             btnMinus.classList.remove('disabled');
