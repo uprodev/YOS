b0cab46e4c4dc0e37971d42b6fbe3e7572b37a0c
derimedved <72937130+derimedved@users.noreply.github.com>
2024-02-05 12:49:28 +0100

    update

--- theme/yos/woocommerce/cart/cart-totals.php
+++ theme/yos/woocommerce/cart/cart-totals.php
@@ -18,95 +18,120 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div class="cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
 
-	<?php do_action( 'woocommerce_before_cart_totals' ); ?>
+<?php if ( wc_coupons_enabled() ) { ?>
 
-	<h2><?php esc_html_e( 'Cart totals', 'woocommerce' ); ?></h2>
+    <div class="basket__side-row">
+        <div class="basket__side-promotional-code">
+            <div class="promotional-code">
+                <button class="promotional-code__title button-link"
+                        data-collapse="promotional-code"><span><?= __('маєте промокод?', 'yos');?></span></button>
+                <div class="promotional-code__body" data-collapse-target="promotional-code">
+                    <input type="text" name="coupon_code" class="input" id="code_coupon" value="" />
+                    <button class="button-primary light w-100" name="apply_coupon_code"><?= __('застосувати', 'yos');?></button>
 
-	<table cellspacing="0" class="shop_table shop_table_responsive">
+                    <?php do_action( 'woocommerce_cart_coupon' ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
 
-		<tr class="cart-subtotal">
-			<th><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-			<td data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></td>
-		</tr>
+<?php $addit_prod = recently_viewed_products();
 
-		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
-			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-				<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
-				<td data-title="<?php echo esc_attr( wc_cart_totals_coupon_label( $coupon, false ) ); ?>"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
-			</tr>
-		<?php endforeach; ?>
+if($addit_prod):
+    $_prod =  wc_get_product( $addit_prod );
+    $br = get_the_terms($addit_prod, 'pa_brand');?>
+    <div class="basket__side-row recently-row">
+        <h4 class="basket__side-title title-4"><?= __('додати до замовлення', 'yos');?></h4>
 
-		<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+        <div class="basket__side-offer">
+            <div class="product-card-sm">
+                <div class="product-card-sm__left">
+                    <a href="<?= get_permalink($addit_prod);?>" class="product-card-sm__img">
+                        <img src="<?= get_the_post_thumbnail_url($addit_prod, 'thumb');?>" alt="">
+                    </a>
+                </div>
+                <div class="product-card-sm__right">
+                    <div class="product-card-sm__title"><a href="<?= get_term_link($br[0]->term_id);?>"><?= $br[0]->name;?></a></div>
+                    <div class="product-card-sm__text">
+                        <div class="product-card-sm__text-1">
+                            <?= get_the_title($addit_prod);?>
+                        </div>
+                        <div class="product-card-sm__text-2">
+                            <?php the_field('seria', $addit_prod);?>
+                        </div>
+                    </div>
+                    <div class="product-card-sm__price">
+                        <?= $_prod->get_price_html();?>
+                    </div>
+                    <div class="product-card-sm__btn-add">
+                        <?php if($_prod->is_type('variable')):?>
+                            <a href="<?= get_permalink($addit_prod);?>"  class="button-primary dark button-primary--sm w-100" ><?= __('додати', 'yos');?></a>
+                        <?php else:?>
+                            <a href="#" data-product_id="<?= $addit_prod;?>" class="button-primary dark button-primary--sm w-100 add-cart addit-add" ><?= __('додати', 'yos');?></a>
+                        <?php endif;?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif;?>
 
-			<?php do_action( 'woocommerce_cart_totals_before_shipping' ); ?>
+    <div class="basket__side-row woocommerce-cart-form  ">
+        <div class="side-basket__payment-info">
+            <!--                            <div class="side-basket__payment-info-row">-->
+            <!--                                <span>--><?php //__('Знижка за системою лояльності', 'yos');?><!--</span>-->
+            <!--                                <span class="text-nowrap">-60 ₴</span>-->
+            <!--                            </div>-->
+            <div class="side-basket__payment-info-row coupon-row" <?= WC()->cart->get_coupons()?'':'style="display:none;"';?>>
+                <span><?= __('Інші знижки', 'yos');?></span>
+                <span class="text-nowrap" >
+                                    -<?= WC()->cart->get_cart_discount_total();?>₴
+                                </span>
+            </div>
+            <div class="side-basket__payment-info-row">
+                <span><?= __('Доставка', 'yos');?></span>
+                <span><?= __('За тарифами перевізника', 'yos');?></span>
+            </div>
+            <div class="side-basket__payment-info-row side-basket__payment-info-row--total">
+                <span><?= __('всього до сплати', 'yos');?></span>
+                <span class="text-nowrap cart-sub"><?php wc_cart_totals_order_total_html(); ?></span>
+            </div>
+        </div>
+    </div>
 
-			<?php wc_cart_totals_shipping_html(); ?>
+    <div class="basket__side-row">
+        <a href="<?= wc_get_checkout_url();?>" class="button-primary dark w-100"><?= __('перейти далі', 'yos');?></a>
+    </div>
 
-			<?php do_action( 'woocommerce_cart_totals_after_shipping' ); ?>
+<?php $sub = WC()->cart->subtotal;
 
-		<?php elseif ( WC()->cart->needs_shipping() && 'yes' === get_option( 'woocommerce_enable_shipping_calc' ) ) : ?>
+if($sub<2000):
 
-			<tr class="shipping">
-				<th><?php esc_html_e( 'Shipping', 'woocommerce' ); ?></th>
-				<td data-title="<?php esc_attr_e( 'Shipping', 'woocommerce' ); ?>"><?php woocommerce_shipping_calculator(); ?></td>
-			</tr>
+    $ost = 2000-$sub;
+    $ost_html = number_format($ost, 2, ',', ' ') . ' '.get_woocommerce_currency_symbol();
+    $percent = round(($ost*100)/2000);
+    $percent_bar = 100-$percent;
 
-		<?php endif; ?>
+    ?>
 
-		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
-			<tr class="fee">
-				<th><?php echo esc_html( $fee->name ); ?></th>
-				<td data-title="<?php echo esc_attr( $fee->name ); ?>"><?php wc_cart_totals_fee_html( $fee ); ?></td>
-			</tr>
-		<?php endforeach; ?>
+    <div class="basket__side-row woocommerce-cart-form">
+        <div class="side-basket__free-shipping">
+            <div class="side-basket__free-shipping-head">
+                <span><?= __('До безкоштовної доставки залишилось:', 'yos');?></span>
+                <span class="text-nowrap"><?= $ost_html;?></span>
+            </div>
+            <div class="side-basket__free-shipping-line">
+                <div class="line-track">
+                    <div class="line" style="width: <?= $percent_bar;?>%;"></div>
+                </div>
+            </div>
+            <div class="side-basket__free-shipping-total">
+                <span class="text-nowrap">0 ₴</span>
+                <span class="text-nowrap">2 000 ₴</span>
+            </div>
+        </div>
+    </div>
 
-		<?php
-		if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
-			$taxable_address = WC()->customer->get_taxable_address();
-			$estimated_text  = '';
-
-			if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
-				/* translators: %s location. */
-				$estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
-			}
-
-			if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
-				foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-					?>
-					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-						<th><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-						<td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
-					</tr>
-					<?php
-				}
-			} else {
-				?>
-				<tr class="tax-total">
-					<th><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-					<td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
-				</tr>
-				<?php
-			}
-		}
-		?>
-
-		<?php do_action( 'woocommerce_cart_totals_before_order_total' ); ?>
-
-		<tr class="order-total">
-			<th><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
-			<td data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>"><?php wc_cart_totals_order_total_html(); ?></td>
-		</tr>
-
-		<?php do_action( 'woocommerce_cart_totals_after_order_total' ); ?>
-
-	</table>
-
-	<div class="wc-proceed-to-checkout">
-		<?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
-	</div>
-
-	<?php do_action( 'woocommerce_after_cart_totals' ); ?>
-
-</div>
+<?php endif;?>
