0dd68d0e7defee7e315857fe783bc7c20bcc3d23
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-02-21 10:53:47 +0200

    style.css app.js updated

--- theme/yos/js/app.js
+++ theme/yos/js/app.js
@@ -1833,10 +1833,7 @@ if (banners.length) {
             if (document.documentElement.clientWidth <= 767 && slider.dataset.mobile == 'false') {
                 linksSlider = new Swiper(slider, {
                     slidesPerView: 'auto',
-                    //freeMode: true,
-                    scrollbar: {
-                        el: slider.querySelector('.swiper-scrollbar'),
-                    },
+                    touchRatio: 0
                 });
 
                 slider.dataset.mobile = 'true';
@@ -1864,15 +1861,39 @@ if (banners.length) {
             slidesPerView: 1,
             spaceBetween: 0,
             speed: 400,
-            touchRatio: 0,
-            // thumbs: {
-            //     swiper: linksSlider,
-            // },
+            slideToClickedSlide: true,
+            scrollbar: {
+                el: images.querySelector('.swiper-scrollbar'),
+                draggable: true,
+            },
+            on: {
+                init: () => {
+                    linksSlider?.slides[0]?.classList.add('active');
+                },
+                slideChange: (e) => {
+                    linksSlider?.slideTo(e.activeIndex);
+                    linksSlider?.slides[e.activeIndex]?.classList.add('active');
+                    linksSlider?.slides.forEach((slide, index) => {
+                        if (index == e.activeIndex) return;
+                        slide.classList.remove('active');
+                    })
+                }
+            }
         });
 
-        if(linksSlider?.controller) {
-            linksSlider.controller.control = swiperSlider
-        }
+        const scrollbar = images.querySelector('.swiper-scrollbar');
+        if (!scrollbar) return;
+        const clickLine = document.createElement('div');
+        clickLine.className = 'click-line';
+        scrollbar.append(clickLine);
+
+        clickLine.addEventListener('pointerdown', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            const countOfSlides = swiperSlider.slides.length;
+            const step = clickLine.clientWidth / countOfSlides
+            swiperSlider.slideTo(Math.floor(e.layerX / step));
+        })
 
         const triggers = banner.querySelectorAll('.category-links__list [data-action="change-banner-image-by-index"]');
 
@@ -1881,7 +1902,9 @@ if (banners.length) {
             if (!index) return;
 
             trigger.addEventListener('mouseenter', () => {
-                swiperSlider.slideTo(index);
+                if (!this.utils.isMobile()) {
+                    swiperSlider.slideTo(index);
+                }
             })
         })
     })
