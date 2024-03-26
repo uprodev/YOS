b7e17d6a2dcbf1cd6ab2269ba0c97f3fe4f9759f
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-03-26 15:58:10 +0100

    Update brands.php

--- theme/yos/templates/sections/brands.php
+++ theme/yos/templates/sections/brands.php
@@ -6,7 +6,7 @@ $brands = get_sub_field('brands');
 
 <div class="top-space-80 top-space-md-150">
     <section class="ticker-logos" data-ticker-logos>
-        <div dir="ltr" class="swiper">
+        <div class="swiper">
             <div class="swiper-wrapper">
                 <?php foreach ($brands as $brand):
                     $name = get_term( $brand );
@@ -17,22 +17,8 @@ $brands = get_sub_field('brands');
                         </a>
                     </div>
                 <?php endforeach;?>
-
-            </div>
-        </div>
-        <div dir="rtl" class="swiper">
-            <div class="swiper-wrapper">
-                <?php foreach ($brands as $brand):
-                    $name = get_term( $brand );
-                    $logo = get_field('logo_brand', 'pa_brand_'.$brand);?>
-                    <div class="swiper-slide">
-                        <a href="<?= get_term_link($brand);?>" class="ticker-logo">
-                            <img src="<?= $logo;?>" alt="<?= $brand->name;?>">
-                        </a>
-                    </div>
-                <?php endforeach;?>
-
             </div>
         </div>
     </section>
 </div>
+
