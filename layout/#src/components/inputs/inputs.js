a6acb966e43e92fbc364b0a064675d06359007da
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-04-02 16:42:25 +0300

    catalog page updated

--- layout/#src/components/inputs/inputs.js
+++ layout/#src/components/inputs/inputs.js
@@ -1,39 +1,43 @@
 
-// const inputsWrap = document.querySelectorAll('[data-input]');
-// if (inputsWrap.length) {
-//     inputsWrap.forEach(inputWrap => {
-//         const input = inputWrap.querySelector('input');
+const inputsWrap = document.querySelectorAll('[data-input]');
+if (inputsWrap.length) {
+    inputsWrap.forEach(inputWrap => {
+        const input = inputWrap.querySelector('input');
 
-//         if(input.required) {
-//             inputWrap.classList.add('required')
-//         }
+        if(!input.closest('.wpcf7-form-control-wrap')) return;
 
-//         input.addEventListener('focus', () => {
-//             inputWrap.classList.add('using');
-//         });
+        if(input.required) {
+            inputWrap.classList.add('required')
+        }
 
-//         input.addEventListener('blur', (e) => {
-//             if (input.value.length === 0) inputWrap.classList.remove('using');
-//         });
-//     })
-// }
+        input.addEventListener('focus', () => {
+            inputWrap.classList.add('using');
+        });
+
+        input.addEventListener('blur', (e) => {
+            if (input.value.length === 0) inputWrap.classList.remove('using');
+        });
+    })
+}
 
 // document.addEventListener('focus', (e) => {
 //     console.log(e.target);
 // })
 
 
-// const textareasWrap = document.querySelectorAll('[data-textarea]');
-// if (textareasWrap.length) {
-//     textareasWrap.forEach(textareaWrap => {
-//         const textarea = textareaWrap.querySelector('textarea');
+const textareasWrap = document.querySelectorAll('[data-textarea]');
+if (textareasWrap.length) {
+    textareasWrap.forEach(textareaWrap => {
+        const textarea = textareaWrap.querySelector('textarea');
 
-//         textarea.addEventListener('focus', () => {
-//             textareaWrap.classList.add('using');
-//         });
+        if(!textarea.closest('.wpcf7-form-control-wrap')) return;
+        
+        textarea.addEventListener('focus', () => {
+            textareaWrap.classList.add('using');
+        });
 
-//         textarea.addEventListener('blur', (e) => {
-//             if (textarea.value.length === 0) textareaWrap.classList.remove('using');
-//         });
-//     })
-// }
+        textarea.addEventListener('blur', (e) => {
+            if (textarea.value.length === 0) textareaWrap.classList.remove('using');
+        });
+    })
+}
